eca85fdda432d665abc94e0a8e595d277eb40e57
Stephen Reidyck <stefaanriedijk@hotmail.com>
2023-07-22 18:24:39 +0200

    /workout-programs met kaart van flowbite

--- src/pages/workout-programs/index.js
+++ src/pages/workout-programs/index.js
@@ -1,76 +1,83 @@
 import React from 'react'
-import Navbar from '../../components/Navbar2'
-import Cardcopy from '../../components/Cardcopy'
-import { createClient } from 'contentful'
+
 import { useRouter } from 'next/router'
+import { createClient } from 'contentful'
+
+import Navbar from '../../components/Navbar2'
+import NewCard from '../../components/NewCard'
 
 
 
 export async function getStaticProps() {
 
-  const contentful = require('contentful')
+    const contentful = require('contentful')
 
-  const client = createClient({
-    space: process.env.DB_SPACE_ID,
-    accessToken: process.env.DB_ACCESS_TOKEN,
-  })
+    const client = createClient({
+      space: process.env.DB_SPACE_ID,
+      accessToken: process.env.DB_ACCESS_TOKEN,
+    })
 
-  const res = await client.getEntries({ content_type: 'workoutProgram' })
+    const res = await client.getEntries({ content_type: 'workoutProgram' })
 
-  return {
-    props : {
-      programs: res.items
-    }
-  }
-}
-
-
-function WorkoutPrograms({ programs }) {
-
-    console.log({programs})
-    
-    const router = useRouter()
-    
-const currentPath = router.route
-console.log('het huidige gekke padje is: ' + currentPath)
-
-  return (
-    <>
-    <main>
-   <Navbar />
-   <div className='text-3xl container'>
-   </div>
-   <div className='container grid grid-cols-3 gap-4 content-center align-top py-7'>
-    <div className='mx-4 '>
-        Lorem Ipsum Hebban Alla Vologe
-    </div>
-    <div className='mx-4 '>
-        Lorem Ipsum Hebban Alla Vologe
-    </div>
-    <div className='mx-4 '>
-        Lorem Ipsum Hebban Alla Vologe
-    </div>
-   </div>
-
-   <div className='container grid grid-cols-3 gap-4 content-center align-top py-7 mx-20'>
-    {programs.map((program)=>
-    {
-        return <div key={program.id}>
-          <Cardcopy
-
-            cardTitle={program.fields.programName}
-            cardDescription={program.fields.description}
-            cardSubheader={'Duration: ' + program.fields.duration + ' weeks'}
-            cardImageUrl={program.fields.image.fields.file.url}
-            cardUrl={currentPath + '/' + program.fields.slug} />
-          <p>{console.log(program.id)}</p>
-        </div>
+    return {
+      props : {
+        programs: res.items
       }
-    )}
-    </div>
-    </main>
-    </>
-  )
+    }
 }
 
-export default WorkoutPrograms
+
+
+
+function WorkoutProgrammers({ programs }) {
+
+
+    //console.log({programs})
+    const router = useRouter()
+    const currentPath = router.route
+    //console.log('het huidige gekke padje is: ' + currentPath)
+
+
+    return (
+      
+      <main>
+        <Navbar />
+
+        <div className='absolute mx-3'>
+
+                <div className='relative w-full grid grid-cols-3 gap-4 content-center align-top py-7'>
+                  <div className='mx-4 '>
+                    Lorem Ipsum Hebban Alla Vologe
+                  </div>
+                  <div className='mx-4 '>
+                    Lorem Ipsum Hebban Alla Vologe
+                  </div>
+                  <div className='mx-4 '>
+                    Lorem Ipsum Hebban Alla Vologe
+                  </div>
+                </div>
+
+            <div className='grid grid-cols-2 md:grid-cols-3 gap-4 items-stretch justify-items-center mx-4 md:mx-10 relative w-full'>
+                  {programs.map((program)=>
+                {
+                  return <div key={program.id}>
+                      <NewCard
+
+                        cardTitle={program.fields.programName}
+                        cardDescription={program.fields.description}
+                        cardSubheader={'Duration: ' + program.fields.duration + ' weeks'}
+                        cardImageUrl={'https:' + program.fields.image.fields.file.url}
+                        cardUrl={currentPath + '/' + program.fields.slug} />
+                                  <p>{console.log(program.id)}</p>
+                    </div>
+                  }
+                  )}
+            </div>
+        </div>
+
+
+    </main>
+    )
+}
+
+export default WorkoutProgrammers
