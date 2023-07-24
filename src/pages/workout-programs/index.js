40aabe53d754ff764e43cc82ca81cedf363902b3
Stephen Reidyck <stefaanriedijk@hotmail.com>
2023-07-24 03:47:50 +0200

    static props veranderd naar serverside

--- src/pages/workout-programs/index.js
+++ src/pages/workout-programs/index.js
@@ -8,7 +8,7 @@ import NewCard from '../../components/NewCard'
 
 
 
-export async function getStaticProps() {
+export async function getServerSideProps() {
 
     const contentful = require('contentful')
 
