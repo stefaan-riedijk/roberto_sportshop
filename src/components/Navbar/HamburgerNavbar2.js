ea93d7fc62ebd9fd2878a58b07cbe7b1b6eabeb6
Stephen Reidyck <stefaanriedijk@hotmail.com>
2023-09-19 00:42:14 +0200

    betere navdims

--- src/components/Navbar/HamburgerNavbar2.js
+++ src/components/Navbar/HamburgerNavbar2.js
@@ -16,11 +16,11 @@ function HamburgerNavbar() {
           )}
         </button>
         <div
-          className={`fixed bottom-60 top-20 z-40 w-1/3 rounded-bl-xl bg-blue-500 text-center transition-all duration-500 ease-in-out ${
+          className={`fixed bottom-60 top-20 z-40 w-1/4 rounded-l-xl bg-blue-500 text-center transition-all duration-500 ease-in-out ${
             isNavOpen ? " right-0" : " right-[-100%]"
           }`}
         >
-          <ul>
+          <ul className="py-8 pr-7 text-right text-lg ">
             <li>Iene</li>
             <li>Miene</li>
             <li>Mutten</li>
