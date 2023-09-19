54d2369539af71c5993fd3ea78e4b877e6cf36e8
Stephen Reidyck <stefaanriedijk@hotmail.com>
2023-09-19 13:36:07 +0200

    met de blog section van flowbite

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Navbar from "../../components/Navbar/Navbar3";
+import BlogSection from "./BlogSection";
 
 function Blog() {
   return (
     <>
       <Navbar />
+      <div className="container mx-auto">
+        <BlogSection />
+      </div>
     </>
   );
 }
